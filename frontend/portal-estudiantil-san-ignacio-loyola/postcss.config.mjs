0a70045b9c0859c1ef178e8d78766dd2ca225f6b
KennySth <kenny_sthiven@outlook.com>
2025-11-08 21:59:53 -0500

    fix: postcss arreglado.

--- frontend/portal-estudiantil-san-ignacio-loyola/postcss.config.mjs
+++ frontend/portal-estudiantil-san-ignacio-loyola/postcss.config.mjs
@@ -1,15 +1,5 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import postcssPresetEnv from 'postcss-preset-env'; // Import postcss-preset-env
-
 export default {
-  plugins: [
-    tailwindcss('./tailwind.config.js'), // Explicitly pass the config file
-    postcssPresetEnv({
-      features: {
-        'focus-within-pseudo-class': false, // Disable if causing issues
-      },
-    }),
-    autoprefixer,
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
